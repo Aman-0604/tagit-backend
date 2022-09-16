ba958143635eb47d8b545d3725d1e864ca46c3c9
Aman Gupta <techbugaman@gmail.com>
2022-09-16 21:43:22 +0530

    Fixed a typo

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const PostsSchema = new Schema({
-    user: {// this will link user module to notes module so as to make notes visible to "only" the user of that notes not someone else. This is like a foriegn key in SQL.
+    user: {// this will link user module to posts module so as to make posts visible to "only" the user of that notes not someone else. This is like a foriegn key in SQL.
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
     },
