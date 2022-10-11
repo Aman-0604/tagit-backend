116b4ca9226023198de42d81ec9a60da1cfc8f8a
Aman Gupta <techbugaman@gmail.com>
2022-10-11 12:00:36 +0530

    Added profileUrl in the users schema

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 
 // JWT enables a very secure verification between user and backend/server
 const JWT_Secret = "Amanisagoodbb$oy";// Idealy you should keep this safe not here. You may keep it in .env.variable file
-const fetch_user=require('../middleware/fetchUser');
+const fetch_user = require('../middleware/fetchUser');
 
 
 // Route 1 : Create a User using : POST "/api/auth/createUser". No login required
@@ -16,6 +16,7 @@ const fetch_user=require('../middleware/fetchUser');
 router.post('/createUser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
+    // body('profileUrl', 'Url must be atleast 3 characters').isLength({ min: 3 }),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
     body('status', 'Status must be atleast 3 characters').isLength({ min: 3 }),
     body('college', 'College must be atleast 3 characters').isLength({ min: 3 }),
@@ -27,16 +28,17 @@ router.post('/createUser', [
     }
     // Check whether the user with this email already exists
     try {
-        let success =false;
+        let success = false;
         let user = await User.findOne({ email: req.body.email });
         if (user) {
-            return res.status(400).json({ success,error: 'Sorry a user with this email already exists' });
+            return res.status(400).json({ success, error: 'Sorry a user with this email already exists' });
         }
         const salt = await bcrypt.genSalt(10);
         const secured_password = await bcrypt.hash(req.body.password, salt);
         // Create a new user
         user = await User.create({
             name: req.body.name,
+            profileUrl: "https://firebasestorage.googleapis.com/v0/b/tagit-71258.appspot.com/o/posts%2Fuser_profile_default.pngac330b38-f3bc-4e58-bd7f-3d21c784995c?alt=media&token=00bcfac6-2c05-434c-b5c9-9faeca3fefb3",
             password: secured_password,
             email: req.body.email,
             status: req.body.status,
@@ -47,11 +49,11 @@ router.post('/createUser', [
                 id: user.id
             }
         }
-        success=true;
+        success = true;
         const auth_token = jwt.sign(data, JWT_Secret);
         // console.log(jwtData);
         // res.json(user)
-        res.json({ success,auth_token });
+        res.json({ success, auth_token });
 
 
     } catch (error) {
@@ -72,8 +74,8 @@ router.post('/login', [
         return res.status(400).json({ errors: errors.array() });
     }
     try {
-        let success =false;
-        let user = await User.findOne({ email:req.body.email });//returns true or falase //I am in (ES6>=) hence I can write {email} instead of {email: email} if both the names are same
+        let success = false;
+        let user = await User.findOne({ email: req.body.email });//returns true or falase //I am in (ES6>=) hence I can write {email} instead of {email: email} if both the names are same
         if (!user) {
             return res.status(400).json('Invalid Credentials'); // I will show this not invalid email. Bcoz I don't want to tell the user that what is incorrect as the user might be a hacker
         }
@@ -86,9 +88,9 @@ router.post('/login', [
                 id: user.id
             }
         }
-        success=true;
+        success = true;
         const auth_token = jwt.sign(data, JWT_Secret);
-        res.json({success, auth_token });
+        res.json({ success, auth_token });
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Internal Server Error Occurred');
@@ -97,13 +99,13 @@ router.post('/login', [
 
 
 // Route 3 : Get logged in User details using POST : "api/auth/getUser" . Login Required
-router.post('/getUser',fetch_user,async (req, res) => {
+router.post('/getUser', fetch_user, async (req, res) => {
     // MiddleWare ek function hota hai jo ki tab tab call hoga jab bhi aapke login vaale route  pe request aayegi
     // We make middleware so as to make our project scaleable. For eg-> if user want to go to payment or blog or shop
     // on the same website then he should not login again and again hence this thing will be handled by a middleware.
 
     try {
-        userId=req.user.id;
+        userId = req.user.id;
         const user = await User.findById(userId).select("-password");// Fetch every data instead of password
         res.send(user);
     } catch (error) {
@@ -115,13 +117,14 @@ router.post('/getUser',fetch_user,async (req, res) => {
 // Route 4 : Update user details using : PUT "/api/auth/updateUser". Login required
 router.put('/updateUser', fetch_user, async (req, res) => {
     try {
-        userId=req.user.id;
-        const { status, college } = req.body;//destructuring
+        userId = req.user.id;
+        const { status, college, profileUrl } = req.body;//destructuring
         const newUser = {};
         if (status) { newUser.status = status };//agar request me status aa raha hai toh usse newUser ke status ke barabar kar do and agar nahi aa raha hai toh iska matlab user update nahi kar raha
         if (college) { newUser.college = college };
+        if (profileUrl) { newUser.profileUrl = profileUrl };
 
-        if (userId !==req.user.id) {
+        if (userId !== req.user.id) {
             return res.status(401).send('Not Allowed');
         }
         const user = await User.findByIdAndUpdate(userId, { $set: newUser }, { new: true });// The default is to return the original, unaltered document. If you want the new, updated document to be returned you have to pass an additional argument: an object with the new property set to true.
